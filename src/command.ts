ab65a4c80b476a00f281d0fdb4e0f81f6d95a4d8
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2023-08-19 22:36:36 +0900

    undo/redoの実現 コマンドパターンとは少し異なるような。 またundoメソッドが死んでいることも気になる。

--- src/command.ts
+++ src/command.ts
@@ -17,8 +17,14 @@ export class AddShapeCommand implements Command {
 
 // コマンドの内容を実装する具象クラス
 export class RemoveShapeCommand implements Command {
-  execute(){}
-  undo(){}
+  private drawingArea: DrawingArea;
+  constructor(drawingArea: DrawingArea) {
+    this.drawingArea = drawingArea;
+  }
+  execute() {
+    this.drawingArea.removeShape();
+  }
+  undo(){};
 }
 
 export class DrawingArea {
@@ -32,17 +38,23 @@ export class DrawingArea {
 }
 
 class CommandManager {
+  // コマンドの実行履歴を管理する配列
   private history: Command[] = [];
+  // undo/redo用のスタック
   private redoStack: Command[] = [];
 
   execute(command: Command): void {
     command.execute();
+    // 実行したコマンドをhistory配列に追加
     this.history.push(command);
+    // 新しいコマンドが実行された時にredoの履歴を破棄
     this.redoStack = [];
   }
 
   undo(): void {
+    // 最後に実行したコマンドを取り出す
     const command = this.history.pop();
+    // コマンドがあれば実行
     if (command) {
       command.undo();
       this.redoStack.push(command);
@@ -59,24 +71,37 @@ class CommandManager {
 }
 
 export class CAD {
-  private readonly _rectangleButton : HTMLButtonElement;
+  private readonly _addShapeButton : HTMLButtonElement;
+  private readonly _removeShapeButton : HTMLButtonElement;
   private readonly _undoButton : HTMLButtonElement;
+  private readonly _redoButton : HTMLButtonElement;
   private commandManager: CommandManager;
 
   constructor(document: Document) {
-    this._rectangleButton = document.getElementById('rectangleButton') as HTMLButtonElement;
+    this._addShapeButton = document.getElementById('addShapeButton') as HTMLButtonElement;
+    this._removeShapeButton = document.getElementById('removeShapeButton') as HTMLButtonElement;
     this._undoButton = document.getElementById('undoButton') as HTMLButtonElement;
+    this._redoButton = document.getElementById('redoButton') as HTMLButtonElement;
 
     this.commandManager = new CommandManager();
 
-    this._rectangleButton.addEventListener('click', () => {
+    this._addShapeButton.addEventListener('click', () => {
       const drawingArea = new DrawingArea();
       const addShapeCommand = new AddShapeCommand(drawingArea);
       this.commandManager.execute(addShapeCommand);
     });
 
+    this._removeShapeButton.addEventListener('click', () => {
+      const drawingArea = new DrawingArea();
+      const removeShapeCommand = new RemoveShapeCommand(drawingArea);
+      this.commandManager.execute(removeShapeCommand);
+    });
+
     this._undoButton.addEventListener('click', () => {
       this.commandManager.undo();
+    });
+
+    this._redoButton.addEventListener('click', () => {
       this.commandManager.redo();
     });
   }
